626161279e276fd5a01abac988be332d209f29d8
Alex <alexjrussian@gmail.com>
2020-05-05 11:51:45 -0400

    refactored comments, added code comments, linted/prettified

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -5,50 +5,56 @@ const Time = require('../../models/Time');
 
 module.exports = {
   Mutation: {
-    async postComment(_, { timeId, body }, context){
+    // ---- POST COMMENT ---- //
+    async postComment(_, { timeId, body }, context) {
       const { username } = checkAuth(context);
 
-      if(body.trim() === '') {
+      // check for empty comment body
+      if (body.trim() === '') {
         throw new UserInputError('Empty comment', {
           errors: {
-            body: 'Comment body must not be empty'
-          }
-        })
+            body: 'Comment body must not be empty',
+          },
+        });
       }
-      const time = await Time.findById(timeId);
 
-      if(time) {
+      // search for time in database
+      const time = await Time.findById(timeId);
+      if (time) {
         time.comments.unshift({
           username,
           body,
-          createdAt: new Date().toISOString()
+          createdAt: new Date().toISOString(),
         });
-
         await time.save();
         return time;
-      } else {
-        throw new UserInputError('Time not found')
       }
+
+      // if the time doesn't exist throw error
+      throw new UserInputError('Time not found');
     },
+
+    // ---- DELETE COMMENT ---- //
     async deleteComment(_, { timeId, commentId }, context) {
+      // confirm the user is signed in
       const { username } = checkAuth(context);
 
+      // make sure the time exists
       const time = await Time.findById(timeId);
-
-      if(time) {
+      if (time) {
         const commentIndex = time.comments.findIndex(c => c.id === commentId);
 
-        if(time.comments[commentIndex].username === username) {
+        if (time.comments[commentIndex].username === username) {
+          // remove the comment from the comments array
           time.comments.splice(commentIndex, 1);
-
           await time.save();
           return time;
-        } else {
-          throw new AuthenticationError('Action not allowed');
         }
+        // throw error if the user tries to delete a comment that is not theirs
+        throw new AuthenticationError('Action not allowed');
       } else {
         throw new UserInputError('Time does not exist');
       }
-    }
-  }
-}
+    },
+  },
+};
